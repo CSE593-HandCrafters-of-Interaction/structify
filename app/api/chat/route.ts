d6a25901386d875fb29e95d5d4902aed9f8842ab
SihanXU <sihanxu@umich.edu>
2025-11-26 01:51:37 +0000

    support gemini backend

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,8 @@
-import { openai } from "@ai-sdk/openai";
+import { google } from "@ai-sdk/google";
 import { streamText, UIMessage, convertToModelMessages } from "ai";
 import chatPrompt from "@/data/chatPrompt.json";
 
-const CHAT_MODEL = openai("gpt-5-mini");
+const CHAT_MODEL = google("gemini-3-pro-preview");
 const systemPrompt = chatPrompt.lines.join("\n");
 
 type ChatRequestBody = {
@@ -12,6 +12,7 @@ type ChatRequestBody = {
 
 export async function POST(req: Request) {
   const { messages, userStudyMode = true }: ChatRequestBody = await req.json();
+
   const result = streamText({
     model: CHAT_MODEL,
     messages: convertToModelMessages(messages),
